9b82ed607750e63955d2b6ae033e7b27fd733436
yousiefaz <yousiefaboalyazed@gmail.com>
2026-06-23 11:01:12 +0300

    editing next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+
   images: {
     remotePatterns: [
       {

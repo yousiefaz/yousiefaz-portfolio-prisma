afe6d6f49ebbfbfaa7c3c54f41a70af3acabb05f
yousiefaz <yousiefaboalyazed@gmail.com>
2026-06-23 10:37:58 +0300

    update dependencies to latest versions

--- next.config.ts
+++ next.config.ts
@@ -5,20 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-      {
-        protocol: "https",
         hostname: "res.cloudinary.com",
       },
     ],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
   },
 };
 
